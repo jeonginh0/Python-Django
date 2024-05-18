fbaad2d25125c80c0f0ba7958e3d5a544ee7a604
jeonginh0 <stitch010913@gmail.com>
2024-05-18 23:49:26 +0900

    study1

---
+++ udemy01/udemyfor/fizzbuzz.py
@@ -0,0 +1,24 @@
+#FizzBuzz
+
+#내가 작성한 코드
+for i in range (1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+#Udemy teacher code
+target = 100
+for number in range(1, target + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
